59b0dde4a72b380e4fb1cf931696a9cc47e57ff1
Mykola Balan <balann705@gmail.com>
2023-04-08 16:58:49 +0300

    Created getAllOrders flow ;Created OrderWithProductsMapper

--- Glovo/src/main/java/com/example/glovo/repositories/OrderRepository.java
+++ Glovo/src/main/java/com/example/glovo/repositories/OrderRepository.java
@@ -2,9 +2,12 @@ package com.example.glovo.repositories;
 
 import com.example.glovo.models.Order;
 import com.example.glovo.repositories.mappers.OrderMapper;
+import com.example.glovo.repositories.mappers.OrderWithProductsMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class OrderRepository {
@@ -16,5 +19,13 @@ public class OrderRepository {
     }
     public Order getOrder(int id){
        return jdbcTemplate.queryForObject("SELECT *  FROM orders WHERE id="+id ,new OrderMapper());
+    }
+    public List<Order> getAllWithProduct(){
+        String getAllQuery = """
+                SELECT orders.id ,orders.date ,orders.cost ,products.id ,products.name ,products.cost  FROM orders 
+                inner join order_products on orders.id=order_products.order_id 
+                inner join products on prod_id=products.id;
+                """;
+        return jdbcTemplate.query(getAllQuery ,new OrderWithProductsMapper());
     }
 }
---
+++ Glovo/src/main/java/com/example/glovo/repositories/mappers/OrderWithProductsMapper.java
@@ -0,0 +1,28 @@
+package com.example.glovo.repositories.mappers;
+
+import com.example.glovo.models.Order;
+import com.example.glovo.models.Product;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderWithProductsMapper implements RowMapper<Order> {
+    @Override
+    public Order mapRow(ResultSet rs, int rowNum) throws SQLException {
+        List<Product> product = new ArrayList<>();
+        product.add(Product.builder()
+                .id(rs.getInt(1))
+                .name(rs.getString(2))
+                .cost(rs.getDouble(3))
+                .build());
+        return Order.builder()
+                .id(rs.getInt(1))
+                .date(rs.getDate(2))
+                .cost(rs.getDouble(3))
+                .products(product)
+                .build();
+    }
+}
