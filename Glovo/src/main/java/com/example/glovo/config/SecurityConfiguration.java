f1903996ee788e8ada153b9fecc933088d02232a
Mykola Balan <balann705@gmail.com>
2023-05-26 18:17:58 +0300

    Added SecurityFilterChain and UserDetailsService

---
+++ Glovo/src/main/java/com/example/glovo/config/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.example.glovo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin(formLogin -> formLogin.permitAll())
+                .authorizeRequests(authorizeRequests -> authorizeRequests.requestMatchers("/orders").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .logout().permitAll();
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        UserDetails user = User.withUsername("Mykola")
+                .password(encoder.encode("2005"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
